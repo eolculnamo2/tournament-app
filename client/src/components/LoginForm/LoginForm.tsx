2aa452e390ae4d5685775103f9f33b102716a728
eolculnamo2 <rbertram8@gmail.com>
2019-08-13 23:06:37 -0500

    login styling, add passport init, fix mongodb console warning

--- client/src/components/LoginForm/LoginForm.tsx
+++ client/src/components/LoginForm/LoginForm.tsx
@@ -1,25 +1,54 @@
 import React, { useState } from 'react';
-import { ICredentials } from '../../../../constants/interfaces';
+import { ICredentials, ILoginForm } from '../../../../constants/interfaces';
+import { postData } from '../../helpers/api';
 
-function LoginForm(): JSX.Element {
+function LoginForm(props: ILoginForm): JSX.Element {
+  const { goToRegister } = props;
   const [username, setUsername] = useState<string>('');
   const [password, setPassword] = useState<string>('');
 
-  const login = () => {
+  const login = async () => {
     const loginPayload: ICredentials = {
       username,
       password,
     };
+
+    const data = await postData(
+      '/authenticate/login',
+      JSON.stringify(loginPayload)
+    );
+
+    console.log(data);
   };
 
   return (
-    <div>
-      <h3>Login</h3>
-      <div>Username</div>
-      <input onChange={e => setUsername(e.target.value)} type="text" />
-      <div>Password</div>
-      <input onChange={e => setPassword(e.target.value)} type="text" />
-      <button>Login</button>
+    <div className="RegistrationForm">
+      <div className="RegistrationForm__head">
+        <h3 className="RegistrationForm__heading">Login</h3>
+      </div>
+      <div className="RegistrationForm__body">
+        <div className="RegistrationForm__label">Username</div>
+        <input
+          className="RegistrationForm__input"
+          onChange={e => setUsername(e.target.value)}
+          type="text"
+        />
+        <div className="RegistrationForm__label">Password</div>
+        <input
+          className="RegistrationForm__input"
+          onChange={e => setPassword(e.target.value)}
+          type="password"
+        />
+        <button className="RegistrationForm__btn" onClick={login}>
+          Login
+        </button>
+        <p
+          className="RegistrationForm__already-have"
+          onClick={() => goToRegister(false)}
+        >
+          Create New Account
+        </p>
+      </div>
     </div>
   );
 }
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,12 +3,12 @@ import { RegistrationForm, LoginForm } from '../components';
 import '../scss/pages/home.scss';
 
 function Home(): JSX.Element {
-  const [showLogin, setShowLogin] = useState<boolean>(false);
+  const [showLogin, setShowLogin] = useState<boolean>(true);
 
   return (
     <div className="Home">
       {!showLogin && <RegistrationForm goToLogin={setShowLogin} />}
-      {showLogin && <LoginForm />}
+      {showLogin && <LoginForm goToRegister={setShowLogin} />}
     </div>
   );
 }
