60ae45c236f4be4d72f750973f98cf2f3fc7eddc
Logan Wood <lwood3499@gmail.com>
2019-08-22 23:08:14 -0500

    updated README, setup score tracking on FE for competitors

---
+++ client/src/components/Competitor/Competitor.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+interface CompetitorPropTypes {
+  competitor: string;
+  competitorNum: string;
+  competitorScore: number;
+  handleScoreAdjust(e: React.MouseEvent, num: number, comp: string): void;
+}
+
+const Competitor = ({
+  competitor,
+  competitorNum,
+  competitorScore,
+  handleScoreAdjust,
+}: CompetitorPropTypes): JSX.Element => {
+  return (
+    <div>
+      <h3>{competitor}</h3>
+      <p>{competitorScore}</p>
+      <div>
+        <button onClick={e => handleScoreAdjust(e, -1, competitorNum)}>
+          -1
+        </button>
+        <button onClick={e => handleScoreAdjust(e, 1, competitorNum)}>
+          +1
+        </button>
+      </div>
+      <div>
+        <button onClick={e => handleScoreAdjust(e, -2, competitorNum)}>
+          -2
+        </button>
+        <button onClick={e => handleScoreAdjust(e, 2, competitorNum)}>
+          +2
+        </button>
+      </div>
+      <div>
+        <button onClick={e => handleScoreAdjust(e, -3, competitorNum)}>
+          -3
+        </button>
+        <button onClick={e => handleScoreAdjust(e, 3, competitorNum)}>
+          +3
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Competitor;
