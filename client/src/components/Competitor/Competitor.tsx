b75cc7073a93ccf9f2035f500a6351f751a867ec
Logan Wood <lwood3499@gmail.com>
2019-08-22 23:17:59 -0500

    added interface to directory, but issues

--- client/src/components/Competitor/Competitor.tsx
+++ client/src/components/Competitor/Competitor.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// import {ICompetitor} from "../../../../constants/interfaces";
 
 interface CompetitorPropTypes {
   competitor: string;
---
+++ constants/interfaces/ICompetitor.ts
@@ -0,0 +1,8 @@
+// import React from 'react';
+
+// interface CompetitorPropTypes {
+//     competitor: string;
+//     competitorNum: string;
+//     competitorScore: number;
+//     handleScoreAdjust(e: React.MouseEvent, num: number, comp: string): void;
+//   }
