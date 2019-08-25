13fc41d4cff6b5549f592b0f48262984e296194a
Logan Wood <lwood3499@gmail.com>
2019-08-25 16:45:10 -0500

    JudgesTable styles and functionality updates

--- constants/interfaces/ICompetitor.ts
+++ constants/interfaces/ICompetitor.ts
@@ -3,4 +3,6 @@ export default interface ICompetitor {
   competitorNum: string;
   competitorScore: number;
   handleScoreAdjust(e: React.MouseEvent, num: number, comp: string): void;
+  handlePenaltyAdjust(e: React.MouseEvent, num: number, comp: string): void;
+  penaltyCount: number;
 }
