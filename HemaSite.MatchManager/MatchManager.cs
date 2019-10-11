581b92a81c7a3bb168c1968832a526774333c74e
eolculnamo2 <rbertram8@gmail.com>
2019-10-10 20:50:55 -0500

    fix API; Begin Match Manager class library

---
+++ HemaSite.MatchManager/MatchManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HemaSite.MatchManager
+{
+  public interface IMatchManager
+  {
+
+  }
+  public class MatchManager : IMatchManager
+  {
+    public IMatch MyProperty { get; set; }
+    public MatchManager()
+    {
+
+    }
+  }
+}
